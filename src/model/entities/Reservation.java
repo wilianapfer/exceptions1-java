3a6f133baffde9712bef548a374c681e42645a72
Wilian Ferreira <wilianapfer@gmail.com>
2023-01-04 15:44:07 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,7 +1,6 @@
 package model.entities;
 
-import java.text.SimpleDateFormat;
-import java.time.Duration;
+import java.text.SimpleDateFormat; 
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -43,9 +42,20 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); //converte em dias
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Check-out date must be after check-in date";
+		}
+				
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		
+		return null;
 	}
 	
 	@Override 
